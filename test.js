20e458f7362f4d4a41a4a96b709dedc6cf7ffa42
Guilherme Felipe Tyszka <105398117+GuilhermeTyszka@users.noreply.github.com>
2023-12-12 17:14:14 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,31 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iframe com Cookie</title>
+</head>
+<body>
+    <script>
+        // Configuração do cookie
+        document.cookie = "authCookie=seu_valor; SameSite=None; Secure";
 
-const gitlabUrl = 'https://gitlab.com'; // Substitua pela URL do seu GitLab
-const projectId = 'seu-id-do-projeto'; // Substitua pelo ID do seu projeto no GitLab
-const privateToken = 'seu-token-de-acesso'; // Substitua pelo seu token de acesso do GitLab
+        // Exemplo de como você pode fazer uma requisição usando o cookie configurado
+        fetch('sua_url_de_requisicao', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + getCookie('authCookie')
+            }
+        }).then(response => {
+            // Lidere com a resposta da requisição
+        });
 
-const apiBaseUrl = `${gitlabUrl}/api/v4/projects/${projectId}/repository/tree`;
-
-async function getFilesInProject() {
-  try {
-    const response = await axios.get(apiBaseUrl, {
-      headers: {
-        'PRIVATE-TOKEN': privateToken,
-      },
-    });
-
-    // Processar os arquivos e pastas
-    processFiles(response.data, '');
-
-  } catch (error) {
-    console.error('Erro ao obter arquivos do GitLab:', error.message);
-  }
-}
-
-function processFiles(files, currentPath) {
-  for (const file of files) {
-    if (file.type === 'blob') {
-      // Se for um arquivo, faça o que for necessário (por exemplo, salvar o caminho do arquivo)
-      const filePath = path.join(currentPath, file.name);
-      console.log('Caminho do arquivo:', filePath);
-
-      // Aqui você pode fazer algo com o caminho do arquivo, como salvá-lo em um array ou arquivo.
-    } else if (file.type === 'tree') {
-      // Se for uma pasta, recursivamente processar os arquivos dentro dela
-      const folderPath = path.join(currentPath, file.name);
-      processFiles(file.children, folderPath);
-    }
-  }
-}
-
-// Iniciar a execução
-getFilesInProject();
+        // Função para obter um cookie pelo nome
+        function getCookie(name) {
+            const value = `; ${document.cookie}`;
+            const parts = value.split(`; ${name}=`);
+            if (parts.length === 2) return parts.pop().split(';').shift();
+        }
+    </script>
+</body>
+</html>
